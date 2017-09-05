514ec412237890807d1627258a2a3e5bbe433f0c
mackness <macksol@gmail.com>
2017-09-04 17:00:32 -0700

    Add scheme property and test.

--- __tests__.py
+++ __tests__.py
@@ -10,5 +10,16 @@ class Test(unittest.TestCase):
         """test parser initialization"""
         self.assertIsNotNone(Parser('https://test.com'))
 
+    """
+        property tests
+    """
+
+    def test_scheme_property(self):
+        """test the scheme property"""
+        scheme_1 = Parser('http://test.com').scheme
+        scheme_2 = Parser('https://test.com').scheme
+        self.assertEqual(scheme_1, 'http', 'did not retrieve correct scheme')
+        self.assertEqual(scheme_2, 'https', 'did not retrieve correct scheme')
+
 if __name__ == '__main__':
     unittest.main()
--- uriparse/parser.py
+++ uriparse/parser.py
@@ -5,6 +5,11 @@
     https://goo.gl/xFd1Ab
 """
 
+SCHEMES = ['ftp', 'http', 'gopher', 'nntp', 'telnet',
+            'imap', 'wais', 'file', 'mms', 'https', 'shttp',
+            'snews', 'prospero', 'rtsp', 'rtspu', 'rsync', '',
+            'svn', 'svn+ssh', 'sftp','nfs','git', 'git+ssh']
+
 class Parser(object):
     """
         Parser class is responsible for:
@@ -13,3 +18,10 @@ class Parser(object):
     """
     def __init__(self, uri_param):
         self.uri = uri_param
+
+    @property
+    def scheme(self):
+        """return scheme of uri"""
+        scheme = self.uri.split('://')[0]
+        if scheme in SCHEMES:
+            return scheme
